f022ea96fa30039f25d98c279e5a8bba305dcdb2
bIpOlAr <117316947+yPyetroXP@users.noreply.github.com>
2024-05-23 21:59:54 -0300

    #1 Added Main.lua

---
+++ main.lua
@@ -0,0 +1,27 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Pika Script | Desenvolvido por @fxannn.exe", HidePremium = false, SaveConfig = true, ConfigFolder = "PikaScript"})
+
+--[[
+Name = <string> - The name of the UI.
+HidePremium = <bool> - Whether or not the user details shows Premium status or not.
+SaveConfig = <bool> - Toggles the config saving in the UI.
+ConfigFolder = <string> - The name of the folder where the configs are saved.
+IntroEnabled = <bool> - Whether or not to show the intro animation.
+IntroText = <string> - Text to show in the intro animation.
+IntroIcon = <string> - URL to the image you want to use in the intro animation.
+Icon = <string> - URL to the image you want displayed on the window.
+CloseCallback = <function> - Function to execute when the window is closed.
+]]
+
+local Tab = Window:MakeTab({
+	Name = "Início",
+	Icon = "rbxassetid://17591781985",
+	PremiumOnly = false
+})x
+
+--[[
+Name = <string> - The name of the tab.
+Icon = <string> - The icon of the tab.
+PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
+]]
